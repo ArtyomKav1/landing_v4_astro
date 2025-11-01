b3ec5937bbc7188aba493303ca26bb1a6a092e4f
Artyom K <artem.kavun90@gmail.com>
2025-11-01 22:23:46 +0300

    fix deploy

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,30 @@
+
 FROM node:20-alpine AS builder
 
-RUN apk add --no-cache \
-    python3 \
-    make \
-    g++ \
-    vips-dev \
-    && corepack enable && corepack prepare pnpm@latest --activate
+RUN corepack enable && corepack prepare pnpm@latest --activate
 
 WORKDIR /app
-COPY pnpm-lock.yaml package.json ./
+
+COPY package.json pnpm-lock.yaml ./
+
 RUN pnpm install --frozen-lockfile
+
 COPY . .
-RUN pnpm run build
+
+RUN pnpm build
 
 FROM nginx:alpine
 
-COPY .htpasswd /etc/nginx/.htpasswd
+
 COPY --from=builder /app/dist /usr/share/nginx/html
+
+
+RUN apk add --no-cache apache2-utils && \
+    htpasswd -bc /etc/nginx/.htpasswd admin admin
+
+
 COPY nginx.conf /etc/nginx/conf.d/default.conf
 
 EXPOSE 80
+
 CMD ["nginx", "-g", "daemon off;"]
--- src/components/UI/BtnCustomLong.tsx
+++ src/components/UI/BtnCustomLong.tsx
@@ -1,9 +1,9 @@
 export const BtnCustomLong = () => {
   return (
-    <a href="#form" className="cursor-pointer">
-      <div className="h-[104px] transform rounded-[20px] bg-[#8C26EA]/40 px-6 duration-300 hover:bg-[#8C26EA]/30  max-lg:px-4">
+    <a href="#TechnicalAdvantages" className="cursor-pointer">
+      <div className="h-[104px] transform rounded-[20px] bg-[#8C26EA]/40 px-6 duration-300 hover:bg-[#8C26EA]/30 max-lg:px-4">
         <div className="h-[104px] transform rounded-[20px] bg-[#8C26EA]/30 px-6 duration-300 hover:bg-[#8C26EA]/20 max-lg:px-4">
-          <div className="flex h-[104px] transform items-center justify-between rounded-[20px] bg-[#8C26EA]/80 px-6 text-[16px] font-medium text-[#FFFFFF] duration-300  max-lg:text-[14px] max-md:text-[16px] max-sm:text-[12px]">
+          <div className="flex h-[104px] transform items-center justify-between rounded-[20px] bg-[#8C26EA]/80 px-6 text-[16px] font-medium text-[#FFFFFF] duration-300 max-lg:text-[14px] max-md:text-[16px] max-sm:text-[12px]">
             <div className="w-[70%]">Технические преимущества сервиса PravoTech Big Data</div>
 
             <div className="shrink-0">
