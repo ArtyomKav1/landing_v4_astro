edcffc6b76a15532eddd656632b1156b8d2c1bc4
Artyom K <artem.kavun90@gmail.com>
2025-11-03 12:29:33 +0300

    fix alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
+import path from 'path';
 import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [react()], 
   vite: {
     plugins: [tailwindcss()],
-
+    resolve: {
+      alias: {
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@uploads': path.resolve('./public/uploads'),
+      },
+    },
     assetsInclude: ['**/*.svg'],
   },
 });
